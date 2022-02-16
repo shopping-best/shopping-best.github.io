763e9a2ff577ec64af9565c3ed4f5e90aee9fd1b
shopping-best <shoppingbestblog@gmail.com>
2022-02-16 17:54:02 +0900

    도메인명 수정

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  siteUrl: `https://shoppingbest.kr`,
+  siteUrl: `https://a01.shoppingbest.kr`,
   name: '쇼베',
   shortDescription: '쇼핑베스트',
   description: '합리적인 소비를 위해 좋은 상품을 추천해주는 쇼핑베스트입니다.',
