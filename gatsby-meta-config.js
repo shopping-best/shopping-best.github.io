e8b88d0a6497b0a4d15ed33ecbf7714bbda2d2bc
shopping-best <shoppingbestblog@gmail.com>
2022-02-17 10:48:03 +0900

    GA 코드 추가

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -4,6 +4,6 @@ module.exports = {
   shortDescription: '쇼핑베스트',
   description: '합리적인 소비를 위해 좋은 상품을 추천해주는 쇼핑베스트입니다.',
   author: '쇼베',
-  ga: '0', // Google Analytics Tracking ID
+  ga: '3251612820', // Google Analytics Tracking ID
   googleSiteVerification: 'HeTZTafWh8_qREVTXoPZfxdICbp6M9C34nnsZ7YbFf4',
 };
